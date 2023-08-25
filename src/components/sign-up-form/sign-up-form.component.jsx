eb259dbcde378f2e4e1b7294e378d50ba751766b
blossomese <blossomese@gmail.com>
2023-08-25 13:49:01 +0100

    updated

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -5,6 +5,8 @@ import {
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
 
+import './sign-up-form.styles.scss'
+
 const defaultFormFields = {
   displayName: "",
   email: "",
