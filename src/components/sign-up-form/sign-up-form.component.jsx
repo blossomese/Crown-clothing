c7a1ba0c9e835ac49d5974ccb29afca18dd0393d
blossomese <blossomese@gmail.com>
2023-08-25 16:05:45 +0100

    created  the sign up component, the form input, button component

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import FormInput from "../form-input/form-input.component";
+import Button from "../button/button.component";
 import {
   createAuthUserWithEmailAndPassword,
   createUserDocumentFromAuth,
@@ -91,7 +92,7 @@ const SignUpForm = () => {
           value={confirmPassword}
         />
 
-        <button type="submit">Sign Up</button>
+        <Button  type="submit">Sign Up</Button>
       </form>
     </div>
   );
